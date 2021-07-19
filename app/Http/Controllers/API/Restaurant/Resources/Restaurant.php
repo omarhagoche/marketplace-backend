60bfbba8dc62073c3032f102040af7bf0504ee75
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2021-07-19 22:14:16 +0200

    Added distance featuer to resturants apis

--- app/Http/Controllers/API/Restaurant/Resources/Restaurant.php
+++ app/Http/Controllers/API/Restaurant/Resources/Restaurant.php
@@ -17,11 +17,18 @@ class Restaurant extends JsonResource
     {
         return array_merge(
             parent::toArray($request),
-
+            $this->getDistance(),
             $this->getDeliveryPrices()
         );
     }
 
+    private function getDistance()
+    {
+        if (request()->has(['myLon', 'myLat'])) {
+            return  ['distance' =>  app('distance')->getDistance(request()->myLat, request()->myLon, $this->latitude, $this->longitude)];
+        }
+        return [];
+    }
 
 
     private function getDeliveryPrices()
