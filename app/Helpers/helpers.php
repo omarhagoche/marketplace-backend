3de84f06fc33190849729d2e7a181472ab9e5b21
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2021-08-08 01:33:45 +0200

    Added helper get_distance function

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -670,3 +670,28 @@ function send_sms($to, $msg)
 
     return $request;
 }
+
+
+/**
+ * Calculate distance between two points
+ */
+function get_distance($longitude, $latituide, $longitude2, $latituide2)
+{
+    return getDistanceFromLatLonInKm($latituide, $longitude, $latituide2, $longitude2);
+    //return sqrt(
+    //    pow(69.1 * ($latituide - $latituide2), 2) + pow(69.1 * ($longitude2 - $longitude) * COS($latituide / 57.3), 2)
+    //);
+}
+
+function getDistanceFromLatLonInKm($lat1, $lon1, $lat2, $lon2)
+{
+    $radius = 6371; // Radius of the earth in km
+    $dLat = deg2rad($lat2 - $lat1);  // deg2rad below
+    $dLon = deg2rad($lon2 - $lon1);
+    $a =
+        sin($dLat / 2) * sin($dLat / 2) +
+        cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+        sin($dLon / 2) * sin($dLon / 2);
+    $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+    return $radius * $c; // Distance in km
+}
