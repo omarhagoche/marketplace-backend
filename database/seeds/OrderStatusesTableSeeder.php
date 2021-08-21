318f372429eef5082c3fda7418d16f89486ac9b7
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2021-08-21 18:47:28 +0200

    Added new order statuses to seeder , also optmized code

--- database/seeds/OrderStatusesTableSeeder.php
+++ database/seeds/OrderStatusesTableSeeder.php
@@ -12,48 +12,59 @@ class OrderStatusesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('order_statuses')->delete();
-        
-        \DB::table('order_statuses')->insert(array (
-            0 => 
-            array (
+
+        \DB::table('order_statuses')->insert([
+            [
                 'id' => 1,
                 'status' => 'Order Received',
                 'created_at' => '2019-08-30 16:39:28',
                 'updated_at' => '2019-10-15 18:03:14',
-            ),
-            1 => 
-            array (
+            ],
+            [
                 'id' => 2,
                 'status' => 'Preparing',
                 'created_at' => '2019-10-15 18:03:50',
                 'updated_at' => '2019-10-15 18:03:50',
-            ),
-            2 => 
-            array (
+            ],
+            [
                 'id' => 3,
                 'status' => 'Ready',
                 'created_at' => '2019-10-15 18:04:30',
                 'updated_at' => '2019-10-15 18:04:30',
-            ),
-            3 => 
-            array (
+            ],
+            [
                 'id' => 4,
                 'status' => 'On the Way',
                 'created_at' => '2019-10-15 18:04:13',
                 'updated_at' => '2019-10-15 18:04:13',
-            ),
-            4 => 
-            array (
+            ],
+            [
                 'id' => 5,
                 'status' => 'Delivered',
                 'created_at' => '2019-10-15 18:04:30',
                 'updated_at' => '2019-10-15 18:04:30',
-            ),
-        ));
-        
-        
+            ],
+            [
+                'id' => 6,
+                'status' => 'no drivers available',
+                'created_at' => '2019-10-15 18:04:30',
+                'updated_at' => '2019-10-15 18:04:30',
+            ],
+            [
+                'id' => 7,
+                'status' => 'waiting for drivers',
+                'created_at' => '2019-10-15 18:04:30',
+                'updated_at' => '2019-10-15 18:04:30',
+            ],
+            [
+                'id' => 8,
+                'status' => 'waiting for restaurant',
+                'created_at' => '2019-10-15 18:04:30',
+                'updated_at' => '2019-10-15 18:04:30',
+            ],
+        ]);
     }
 }
