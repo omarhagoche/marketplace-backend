d2f6d30000c1487f208a73b93b10872d47fb882a
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2021-08-23 21:16:10 +0200

    Updated activated_at property of users table to nullable

--- database/migrations/2021_08_23_174523_add_activated_at_to_users_table.php
+++ database/migrations/2021_08_23_174523_add_activated_at_to_users_table.php
@@ -14,7 +14,7 @@ class AddActivatedAtToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('activated_at');
+            $table->timestamp('activated_at')->nullable();
         });
     }
 
