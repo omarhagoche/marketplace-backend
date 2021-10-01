b99dca11877d061ea9f4ca4e8864d13f226cdd90
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2021-10-01 16:16:12 +0200

    Optimized statistics manager api to return manager_fee

--- app/Http/Controllers/API/Manager/StatisticAPIController.php
+++ app/Http/Controllers/API/Manager/StatisticAPIController.php
@@ -26,14 +26,23 @@ class StatisticAPIController extends Controller
             return response()->json(["error" => "User not linked to any restauarnt"], 403);
         }
 
-        $settlements = SettlementManager::select(DB::raw("IFNULL(SUM(amount),0) amount"), DB::raw('IFNULL(SUM(count),0) count'))
-            ->where('restaurant_id', $restaurant->id)->first()->toArray();
+        $settlements = SettlementManager::select(
+            DB::raw("IFNULL(SUM(amount),0) amount"),
+            DB::raw("IFNULL(SUM(amount / fee * 100),0) manager_fee"),
+            DB::raw('IFNULL(SUM(count),0) count'),
+        )
+            ->where('restaurant_id', $restaurant->id)
+            ->first()
+            ->makeHidden(['custom_fields'])
+            ->toArray();
+
+        $settlements['manager_fee'] = round($settlements['manager_fee'], 3);
 
 
         $availabel_orders_for_settlement =  FoodOrder::join('foods', 'foods.id', 'food_orders.food_id')
             ->join('orders', 'orders.id', 'food_orders.order_id')
             ->select(
-                DB::raw("IFNULL(SUM(food_orders.quantity * food_orders.price),0) amount"),
+                DB::raw("IFNULL(SUM(food_orders.quantity * food_orders.price),0) manager_fee"),
                 DB::raw('IFNULL(COUNT(DISTINCT food_orders.order_id),0) count')
             )
             ->where('foods.restaurant_id', $restaurant->id)
@@ -41,7 +50,7 @@ class StatisticAPIController extends Controller
             ->whereNull('orders.settlement_manager_id')
             ->first();
 
-        $availabel_orders_for_settlement->amount  = round(($restaurant->admin_commission / 100) *  $availabel_orders_for_settlement->amount, 3);
+        $availabel_orders_for_settlement->amount  = round(($restaurant->admin_commission / 100) *  $availabel_orders_for_settlement->manager_fee, 3);
 
         return [
             'settlements' => $settlements,
