eebd8e8f4fd91c614906d474583dc7f2593e1593
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2022-02-23 20:52:07 +0200

    fixed bug in NoteController to execute route:list command

--- app/Http/Controllers/API/NoteController.php
+++ app/Http/Controllers/API/NoteController.php
@@ -6,26 +6,22 @@ use Illuminate\Http\Request;
 use App\Http\Requests\NoteRequest;
 use App\Http\Controllers\Controller;
 use App\Repositories\NoteRepository;
-use App\Repositories\FoodOrderRepository;
 
 class NoteController extends Controller
 {
     /** @var  noteRepository */
     private $noteRepository;
-    public function __construct(NoteRepository $noteRepo, FoodOrderRepository $foodOrderRepository, CartRepository $cartRepo, PaymentRepository $paymentRepo, NotificationRepository $notificationRepo, UserRepository $userRepository)
+    public function __construct(NoteRepository $noteRepo)
     {
         $this->noteRepository = $noteRepo;
-
     }
     public function store(NoteRequest $request)
     {
         try {
             $this->noteRepository->create($request->all());
             return $this->sendResponse($orders->toArray(), 'Orders retrieved successfully');
-    
         } catch (ValidatorException $e) {
             return $this->sendError($e->getMessage());
         }
-        
     }
 }
