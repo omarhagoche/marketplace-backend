1421c619353a678ade633be5f3950f9311234dc4
hanin ahmid <hano.hyun.hh@gmail.com>
2022-03-07 12:43:22 +0200

    food table -> add excel export

--- resources/views/operations/restaurantProfile/foods/table.blade.php
+++ resources/views/operations/restaurantProfile/foods/table.blade.php
@@ -85,9 +85,16 @@
     </table>
 </div>
 
+@push('css_lib')
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.cssgit ">
+@endpush
 @push('scripts')
 <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
     <script>
         $(document).ready(function() {
             // $('#example').DataTable();
@@ -98,8 +105,13 @@
                 $(this).html( '<input style="width: 100%;" class="form-control" type="text" placeholder="'+title+'" />' );
             
             } );
+  
             // DataTable
             var table = $('#example').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'excel'
+                 ],
                 initComplete: function () {
                     // Apply the search
                     this.api().columns().every( function () {
