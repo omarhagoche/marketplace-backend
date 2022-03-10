87183ce6fb45c8562a651f7c6ae6eb683fd53c0f
hussenk <hussen199710@yahoo.com>
2022-03-10 20:54:06 +0200

    - repeated fields

--- resources/views/operations/drivers/fields.blade.php
+++ resources/views/operations/drivers/fields.blade.php
@@ -27,47 +27,7 @@
         </div>
     </div>
 
-    <!-- User phone_number Field -->
-    <div class="form-group row ">
-        {!! Form::label('phone_number', trans('lang.phone_number'), ['class' => 'col-3 control-label text-right']) !!}
-        <div class="col-9">
-            {!! Form::Text('phone_number', $user->phone_number ?? null, ['class' => 'form-control', 'placeholder' => trans('lang.phone_number_placeholder')]) !!}
-            <div class="form-text text-muted">
-                {{ trans('lang.phone_number_help') }}
-            </div>
-        </div>
-    </div>
-    <!-- User email Field -->
-    <div class="form-group row ">
-        {!! Form::label('email', trans('lang.email'), ['class' => 'col-3 control-label text-right']) !!}
-        <div class="col-9">
-            {!! Form::Email('email', $user->email ?? null, ['class' => 'form-control', 'placeholder' => trans('lang.email_placeholder')]) !!}
-            <div class="form-text text-muted">
-                {{ trans('lang.email_help') }}
-            </div>
-        </div>
-    </div>
 
-    <!-- Driver type ID Field -->
-    <div class="form-group row ">
-        {!! Form::label('driver_type_id', trans('lang.driver_type_id'), ['class' => 'col-3 control-label text-right']) !!}
-        <div class="col-9">
-            {!! Form::select('driver_type_id', $types, null, ['class' => 'select2 form-control']) !!}
-            <div class="form-text text-muted">{{ trans('lang.driver_type_id_help') }}</div>
-        </div>
-    </div>
-
-
-    <!-- 'Boolean Available Field' -->
-    <div class="form-group row ">
-        {!! Form::label('available', trans('lang.driver_available'), ['class' => 'col-3 control-label text-right']) !!}
-        <div class="checkbox icheck">
-            <label class="col-9 ml-2 form-check-inline">
-                {!! Form::hidden('available', 0) !!}
-                {!! Form::checkbox('available', 1, null) !!}
-            </label>
-        </div>
-    </div>
 
 
     {{-- <!-- 'Driver Type Field' -->
@@ -96,13 +56,6 @@
             <label class="col-9 ml-2 form-check-inline">
                 {!! Form::hidden('available', 0) !!}
                 {!! Form::checkbox('available', 1, null) !!}
-            </label>
-        </div>
-        {!! Form::label('active', trans('lang.user_active'), ['class' => 'col-3 control-label text-right']) !!}
-        <div class="checkbox icheck">
-            <label class="col-9 ml-2 form-check-inline">
-                {!! Form::hidden('active', 0) !!}
-                {!! Form::checkbox('active', 1, $user->active ?? 0) !!}
             </label>
         </div>
     </div>
