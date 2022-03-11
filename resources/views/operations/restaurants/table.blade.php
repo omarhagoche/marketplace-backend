5b1a3c0f89e827a13f24792f6f021132205ba491
hussenk <hussen199710@yahoo.com>
2022-03-11 22:19:07 +0200

    search restaurant

--- resources/views/operations/restaurants/table.blade.php
+++ resources/views/operations/restaurants/table.blade.php
@@ -4,20 +4,35 @@
 
 
 @php
-$searchFields = [['name' => 'name', 'data-column' => 1, 'title' => trans('lang.restaurant'), 'type' => 'text', 'value' => request('name')], ['name' => 'address', 'data-column' => 2, 'title' => trans('lang.restaurant_address'), 'type' => 'text', 'value' => request('address')], ['name' => 'phone', 'data-column' => 3, 'title' => trans('lang.restaurant_phone'), 'type' => 'text', 'value' => request('phone')], ['name' => 'moblie', 'data-column' => 4, 'title' => trans('lang.restaurant_mobile'), 'type' => 'text', 'value' => request('moblie')]];
+$searchFields = [['name' => 'name', 'data-column' => 1, 'title' => trans('lang.restaurant'), 'type' => 'text', 'value' => request('name')],
+ ['name' => 'address', 'data-column' => 2, 'title' => trans('lang.restaurant_address'), 'type' => 'text', 'value' => request('address')],
+ ['name' => 'phone', 'data-column' => 3, 'title' => trans('lang.restaurant_phone'), 'type' => 'text', 'value' => request('phone')],
+ ['name' => 'moblie', 'data-column' => 4, 'title' => trans('lang.restaurant_mobile'), 'type' => 'text', 'value' => request('moblie')]];
 @endphp
 
-{!! Form::open(['route' => ['operations.restaurant_profile.index'], 'method' => 'get']) !!}
+{!! Form::open(['route' => ['operations.restaurant_profile.index'],'method' => 'get']) !!}
+
 {{-- Start customer search fields --}}
 {{-- <form id="myCustomeSearchForm" novalidate> --}}
 <div class="form-row">
     @foreach ($searchFields as $f)
         <div class="col-md-3">
             <label for="validationCustom{{ $f['name'] }}">{{ $f['title'] }}</label>
-
-            <input name="{{ $f['name'] }}" type="{{ $f['type'] }}" value="{{ $f['value'] }}"
-                class="form-control searchDTFields" data-column="{{ $f['data-column'] }}"
-                id="validationCustom{{ $f['name'] }}">
+            @if ($f['type'] == 'select')
+                <select name="{{ $f['name'] }}" value="{{ $f['value'] }}" class="form-control searchDTFields"
+                    data-column="{{ $f['data-column'] }}" id="validationCustom{{ $f['name'] }}">
+                    <option value="">{{ trans('lang.order_order_status_id') }}</option>
+                    @foreach ($f['collection'] as $value)
+                        <option value="{{ $value->id }}"
+                            {{ request('order_status') == $value->id ? 'selected' : '' }}>
+                            {{ $value->{$f['property']} }}</option>
+                    @endforeach
+                </select>
+            @else
+                <input name="{{ $f['name'] }}" type="{{ $f['type'] }}" value="{{ $f['value'] }}"
+                    class="form-control searchDTFields" data-column="{{ $f['data-column'] }}"
+                    id="validationCustom{{ $f['name'] }}">
+            @endif
         </div>
     @endforeach
     <div class="col-auto align-self-end">
@@ -26,6 +41,8 @@ $searchFields = [['name' => 'name', 'data-column' => 1, 'title' => trans('lang.r
 </div>
 {{-- </form> --}}
 {!! Form::close() !!}
+{{-- End customer search fields --}}
+
 <hr />
 
 {!! $dataTable->table(['width' => '100%']) !!}
