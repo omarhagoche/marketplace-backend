b376d969eba42a3da3d8e1e13a764017bf1cf157
hanin ahmid <hano.hyun.hh@gmail.com>
2022-03-12 12:17:56 +0200

    fix bug

--- app/Http/Controllers/Operations/RestaurantProfileController.php
+++ app/Http/Controllers/Operations/RestaurantProfileController.php
@@ -213,10 +213,10 @@ class RestaurantProfileController extends Controller
     public function restaurantFoodsDelete($id,$restaurantId) {   
 
         $food = $this->foodRepository->findWithoutFail($id);
-        $order = $this->foodOrderRepository->findByField('food_id',$id);
-        if(!empty($order)) {
+        $order = $this->foodOrderRepository->findByField('food_id',$id)->count();
+        if($order != 0) {
             Flash::error('Food Related To Order');
-
+            
             return redirect(route('operations.restaurant.foods.index',$restaurantId));
         }
         if (empty($food)) {
