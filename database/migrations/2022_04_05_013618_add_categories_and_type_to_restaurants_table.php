9237e528a098cbc32d4f3a4b5d8aa0684252677f
Ghassan Aljaafri <ghassan.aljaafri@gmail.com>
2022-04-12 18:17:32 +0200

    Fix migrations issue - in AddCategoriesAndTypeToRestaurantsTable.php - Up() - Down()

--- database/migrations/2022_04_05_013618_add_categories_and_type_to_restaurants_table.php
+++ database/migrations/2022_04_05_013618_add_categories_and_type_to_restaurants_table.php
@@ -14,7 +14,15 @@ class AddCategoriesAndTypeToRestaurantsTable extends Migration
     public function up()
     {
         Schema::table('restaurants', function (Blueprint $table) {
-            //
+            /**
+             * Merchant type is used to differentiate restaurants from other merchants type like:
+             * Supermarkets
+             * Pharmacies 
+             * ..Etc 
+             */
+            $table->enum('merchant_type', ['RESTAURANT', 'SUPERMARKET','SHOP'])->default('RESTAURANT');	
+            $table->integer('merchant_category_id')->unsigned()->nullable();
+            $table->foreign('merchant_category_id')->references('id')->on('merchant_categories')->onDelete('set null');
         });
     }
 
@@ -26,15 +34,8 @@ class AddCategoriesAndTypeToRestaurantsTable extends Migration
     public function down()
     {
         Schema::table('restaurants', function (Blueprint $table) {
-            /**
-             * Merchant type is used to differentiate restaurants from other merchants type like:
-             * Supermarkets
-             * Pharmacies 
-             * ..Etc 
-             */
-            $table->enum('merchant_type', ['RESTAURANT', 'SUPERMARKET','SHOP'])->default('RESTAURANT');	
-            $table->integer('merchant_category_id')->unsigned()->nullable();
-            $table->foreign('merchant_category_id')->references('id')->on('merchant_categories')->onDelete('set null');
+            $table->dropColumn('merchant_type');
+            $table->dropColumn('merchant_category_id');
         });
     }
 }
